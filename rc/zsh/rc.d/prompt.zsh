fb2d5bbacc75eb8a24a7e52a80c5b56a0d5cfa10
asmz <asmz0@facebook.com>
2015-06-10 11:57:38 +0900

    Modify prompt

--- rc/zsh/rc.d/prompt.zsh
+++ rc/zsh/rc.d/prompt.zsh
@@ -2,9 +2,6 @@ function str_with_color() {
     echo "%{$fg[$1]%}$2%{$reset_color%}"
 }
 
-function my_git_status() {
-    [ $(current_branch) ] && echo "$(git_prompt_info)"
-}
 ZSH_THEME_GIT_PROMPT_PREFIX="(%{$fg[red]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}) "
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}!"
@@ -14,5 +11,5 @@ CURRENT_DIRECTORY=$(str_with_color cyan '%~')
 HOSTNAME=$(str_with_color yellow '%m')
 PROMPT_CHAR_OK='$ '
 PROMPT_CHAR_NG=$(str_with_color red ${PROMPT_CHAR_OK})
-PROMPT='[${CURRENT_DIRECTORY}@${HOSTNAME}] $(my_git_status)
+PROMPT='[${CURRENT_DIRECTORY}@${HOSTNAME}] $(git_prompt_info)
 %(?.${PROMPT_CHAR_OK}.${PROMPT_CHAR_NG})'
