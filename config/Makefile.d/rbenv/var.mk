aa4d123615bd8a7f2ecbd4c3f211dd3759ef5be3
asmz <land.front.high.field@gmail.com>
2019-06-27 18:49:50 +0900

    Delete rbenv

--- config/Makefile.d/rbenv/var.mk
+++
@@ -1,7 +0,0 @@
-RBENV = $(RBENV_DIR)/bin/rbenv
-RBENV_DIR = ~/.rbenv
-RBENV_PLUGIN_DIR = $(RBENV_DIR)/plugins
-RBENV_INSTALLER = https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer
-
-INSTALL_TARGETS += rbenv-install
-UPDATE_TARGETS += rbenv-update
